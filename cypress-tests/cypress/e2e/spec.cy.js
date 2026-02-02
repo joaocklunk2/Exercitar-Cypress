8d96e81505691f3246d15421398cd5a4e8c7a2f0
joaocklunk2 <joaocklunk@gmail.com>
2026-02-02 16:52:37 -0300

    Primeiro teste e GitActions CI

---
+++ cypress-tests/cypress/e2e/spec.cy.js
@@ -0,0 +1,12 @@
+describe('Estudos de Cypress', () => {
+
+  beforeEach(() => {
+    cy.visit('./index.html')
+  })
+  it('passes', () => {
+    cy.get('h1')
+      .contains('Login de Teste')
+      .should('be.visible')      
+  })
+})
+
